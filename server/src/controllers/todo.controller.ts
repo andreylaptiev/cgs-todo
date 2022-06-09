e9db0ea45d6a494e781d711f31483b82ad0fba66
Andrey Laptiev <andreylaptiev13@gmail.com>
2022-06-09 17:25:26 +0300

    feat: Added todo controller methods

--- server/src/controllers/todo.controller.ts
+++ server/src/controllers/todo.controller.ts
@@ -3,10 +3,35 @@ import TodoService from "../services/todo.service";
 
 export class TodoController {
     constructor(private todoService: TodoService) {}
-    async getAllTodo(_: Request, res: Response) {
+    async getAllTodo(_req: Request, _res: Response) {
         // TODO: Write your implementation here
         const todos = await this.todoService.findAll();
-        res.send(todos);
+        return todos;
+    }
+
+    async getOneTodo(req: Request, _: Response) {
+        const { id } = req.params;
+        const todo = await this.todoService.findOne(id);
+        return todo;
+    }
+
+    async createOneTodo(req: Request, _res: Response) {
+        const data = req.body;
+        const todo = await this.todoService.createOne(data);
+        return todo;
+    }
+
+    async updateOneTodo(req: Request, _: Response) {
+        const { id } = req.params;
+        const data = req.body;
+        const todo = await this.todoService.updateOne(id, data);
+        return todo;
+    }
+
+    async deleteOneTodo(req: Request, _: Response) {
+        const { id } = req.params;
+        const todo = await this.todoService.deleteOne(id);
+        return todo;
     }
 }
 
