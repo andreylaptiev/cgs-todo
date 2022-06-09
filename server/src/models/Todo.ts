f7c0a19efda8479dec9d0b8ed781f4cf3e4e18b7
Andrey Laptiev <andreylaptiev13@gmail.com>
2022-06-09 17:30:07 +0300

    feat: Added todo model

---
+++ server/src/models/Todo.ts
@@ -0,0 +1,43 @@
+import { model, Schema } from "mongoose";
+import { body } from "express-validator";
+import ITodo from "../types/todos.type";
+
+const todoSchema = new Schema<ITodo>({
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  isPublic: {
+    type: Boolean,
+    required: true,
+  },
+  isCompleted: {
+    type: Boolean,
+    required: true,
+  }
+});
+
+export const todoSchemaValidation = [
+  body(["title", "description", "year"])
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage("Required field is empty"),
+  body("description")
+    .isLength({ max: 200 })
+    .withMessage("Description max length is 200 symbols"),
+  body("year")
+    .isLength({ min: 4, max: 4 })
+    .withMessage("Year length is 4 symbols"),
+];
+
+const Todo = model<ITodo>("Todo", todoSchema);
+
+export default Todo;
