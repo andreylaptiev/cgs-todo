c7e2738d64bb2245ceebfa0f17dc8db2bcb521f8
Andrey Laptiev <andreylaptiev13@gmail.com>
2022-06-19 09:07:00 +0300

    feat: Updated todo types models services and controllers to work with filter

--- server/src/controllers/todo.controller.ts
+++ server/src/controllers/todo.controller.ts
@@ -1,11 +1,25 @@
 import { Response, Request } from "express";
+import { ITodoFilter } from "todos.type";
 import TodoService from "../services/todo.service";
 
 export class TodoController {
     constructor(private todoService: TodoService) {}
-    async getAllTodo(_req: Request, _res: Response) {
-        // TODO: Write your implementation here
-        const todos = await this.todoService.findAll();
+    async getAllTodo(req: Request, res: Response) {
+        const userId = res.locals.user._id;
+        const filterParams: ITodoFilter = req.query;
+        const title = filterParams.title;
+        const filter: ITodoFilter = filterParams.isPublic === "true" ?
+        {
+            isPublic: filterParams.isPublic,
+            isCompleted: filterParams.isCompleted,
+        } :
+        {
+            userId: userId,
+            isPublic: filterParams.isPublic,
+            isCompleted: filterParams.isCompleted,
+        };
+        if (title) filter.title = { $regex: title, $options: "i" };
+        const todos = await this.todoService.findAll(filter);
         return todos;
     }
 
@@ -15,15 +29,17 @@ export class TodoController {
         return todo;
     }
 
-    async createOneTodo(req: Request, _res: Response) {
-        const data = req.body;
+    async createOneTodo(req: Request, res: Response) {
+        const userId = res.locals.user._id;
+        const data = {...req.body, userId};
         const todo = await this.todoService.createOne(data);
         return todo;
     }
 
-    async updateOneTodo(req: Request, _: Response) {
+    async updateOneTodo(req: Request, res: Response) {
+        const userId = res.locals.user._id;
         const { id } = req.params;
-        const data = req.body;
+        const data = {...req.body, userId};
         const todo = await this.todoService.updateOne(id, data);
         return todo;
     }
--- server/src/types/todos.type.ts
+++ server/src/types/todos.type.ts
@@ -1,11 +1,19 @@
 // TODO: Put a real interfaces here
 
 interface ITodo {
+  userId: string;
   title: string;
   description: string;
   year: string;
   isPublic: boolean;
   isCompleted: boolean;
+}
+
+export interface ITodoFilter {
+  userId?: string;
+  title?: string | object;
+  isCompleted?: string;
+  isPublic?: string;
 }
 
 export default ITodo;
